e35d70063d5e23c6f89a4e52d52c4f7733360f27
Lucas Ferrari Lopes <lf.ferrarilucas@gmail.com>
2024-01-15 17:31:20 -0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'poke-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'pokedex';
+}
